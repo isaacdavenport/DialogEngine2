4de85a1acbaea48204653c7e7de40eeae44dbcfe
lucky1603 <sinisa.ristic@gmail.com>
2020-02-19 19:28:34 +0100

    Assembly info version changed.

--- DialogGenerator/Properties/AssemblyInfo.cs
+++ DialogGenerator/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Windows;
 )]
 
 //This is the version number picked up for  display in debug/settings screen
-[assembly: AssemblyVersion("3.13.*")] 
+[assembly: AssemblyVersion("3.31.*")] 
 
