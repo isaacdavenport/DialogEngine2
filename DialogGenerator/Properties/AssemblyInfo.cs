eb4498b40e8ddadb66337b74d4ca1807d01ea564
lucky1603 <sinisa.ristic@gmail.com>
2020-02-28 11:46:22 +0100

    ComboBox correction - DLGEN-374
    Correction on combo box entry controls for edit phrase view. The entries which are not in the combo box are now accepted.

--- DialogGenerator/Properties/AssemblyInfo.cs
+++ DialogGenerator/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Windows;
 )]
 
 //This is the version number picked up for  display in debug/settings screen
-[assembly: AssemblyVersion("3.36.*")] 
+[assembly: AssemblyVersion("3.37.*")] 
 
