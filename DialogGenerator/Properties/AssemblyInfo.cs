cce6f40547905dc5e4b860bc5eb0b6c95fce742c
Isaac Davenport <isaac@isaacdavenport.com>
2020-03-23 13:03:35 -0600

    Remove outdated RandomSelectionServices.cs

--- DialogGenerator/Properties/AssemblyInfo.cs
+++ DialogGenerator/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Windows;
 )]
 
 //This is the version number picked up for  display in debug/settings screen
-[assembly: AssemblyVersion("3.62.*")] 
+[assembly: AssemblyVersion("3.59.*")] 
 
